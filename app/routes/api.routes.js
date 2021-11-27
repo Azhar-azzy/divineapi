a4eaabc8cea2288a0d8cf9d4aeb649a6bcb9f93e
azhar-azzy <mohammadazhar1301@gmail.com>
2021-11-28 00:41:30 +0530

    make it better

--- app/routes/api.routes.js
+++ app/routes/api.routes.js
@@ -2,7 +2,7 @@ module.exports = (app) => {
   const { authJwt, apiAuth } = require("../middlewares");
   const controller = require("../controllers/api.controller.js");
 
-  var router = require("express").Router();
+  //var router = require("express").Router();
 
   app.use(function (req, res, next) {
     res.header(
@@ -22,26 +22,26 @@ module.exports = (app) => {
     controller.dailyHoroscope
   );
 
-  // Create a new Tutorial
-  router.post("/", controller.create);
+  // // Create a new Tutorial
+  // router.post("/", controller.create);
 
-  // Retrieve all Tutorials
-  router.get("/", controller.findAll);
+  // // Retrieve all Tutorials
+  // router.get("/", controller.findAll);
 
-  // Retrieve all published Tutorials
-  router.get("/published", controller.findAllPublished);
+  // // Retrieve all published Tutorials
+  // router.get("/published", controller.findAllPublished);
 
-  // Retrieve a single Tutorial with id
-  router.get("/:id", controller.findOne);
+  // // Retrieve a single Tutorial with id
+  // router.get("/:id", controller.findOne);
 
-  // Update a Tutorial with id
-  router.put("/:id", controller.update);
+  // // Update a Tutorial with id
+  // router.put("/:id", controller.update);
 
-  // Delete a Tutorial with id
-  router.delete("/:id", controller.delete);
+  // // Delete a Tutorial with id
+  // router.delete("/:id", controller.delete);
 
-  // Delete all Tutorials
-  router.delete("/", controller.deleteAll);
+  // // Delete all Tutorials
+  // router.delete("/", controller.deleteAll);
 
-  app.use("/api/tutorials", router);
+  // app.use("/api/tutorials", router);
 };
